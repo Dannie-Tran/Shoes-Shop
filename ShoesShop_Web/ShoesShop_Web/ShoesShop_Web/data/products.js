0df35b6f4ec05573687e435ee99767325c6af06a
Dat <bedathieugia@gmail.com>
2024-04-21 18:55:26 +0700

    them du an moi

---
+++ ShoesShop_Web/ShoesShop_Web/ShoesShop_Web/data/products.js
@@ -0,0 +1,463 @@
+var list_products = [{
+    "name": "Yeezy 350",
+    "company": "ADIDAS",
+    "img": "img/products/adidas-yeezy-350.png",
+    "price": "3.490.000",
+    "star": 3,
+    "rateCount": 26,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Sam0"
+}, {
+    "name": "ADIDAS Prophere Customize",
+    "company": "ADIDAS",
+    "img": "img/products/adidas-prophere-customize.png",
+    "price": "6.690.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Xia0"
+}, {
+    "name": "ADIDAS Super Star",
+    "company": "ADIDAS",
+    "img": "img/products/adidas-tenisky-super-star.png",
+    "price": "7.690.000",
+    "star": 5,
+    "rateCount": 188,
+    "promo": {
+        "name": "giamgia",
+        "value": "500.000"
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Opp0"
+}, {
+    "name": "ADIDAS Ultraboost",
+    "company": "ADIDAS",
+    "img": "img/products/adidas-ultraboost-4.png",
+    "price": "4.790.000",
+    "star": 5,
+    "rateCount": 7,
+    "promo": {
+        "name": "giamgia",
+        "value": "250.000"
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Nok0"
+}, {
+    "name": "CONVERSE Chuck Taylor",
+    "company": "CONVERSE",
+    "img": "img/products/converse-chuck-taylor.png",
+    "price": "11.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "1.500.000"
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Sam1"
+}, {
+    "name": "NIKE Adapt",
+    "company": "NIKE",
+    "img": "img/products/nike-adapt-bb.png",
+    "price": "31.990.000",
+    "star": 4,
+    "rateCount": 10,
+    "promo": {
+        "name": "giareonline",
+        "value": "27.990.000"
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "App0"
+}, {
+    "name": "NIKE Air Blue",
+    "company": "NIKE",
+    "img": "img/products/nike-air-max-97-blue.png",
+    "price": "4.690.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Opp1"
+}, {
+    "name": "NIKE Air Black",
+    "company": "NIKE",
+    "img": "img/products/nike-air-max-97.png",
+    "price": "6.290.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "500.000"
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Sam2"
+}, {
+    "name": "NIKE Air Max 270",
+    "company": "NIKE",
+    "img": "img/products/nike-air-max-270-react.png",
+    "price": "8.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "App1"
+}, {
+    "name": "NIke Flyknit",
+    "company": "NIKE",
+    "img": "img/products/nike-flyknit.png",
+    "price": "17.000.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "16.990.000"
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "App2"
+}, {
+    "name": "NIKE React",
+    "company": "NIKE",
+    "img": "img/products/nike-react-element.png",
+    "price": "12.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Xia1"
+}, {
+    "name": "ADIDAS Super Red",
+    "company": "ADIDAS",
+    "img": "img/products/adidas-super-star-red.png",
+    "price": "3.490.000",
+    "star": 4,
+    "rateCount": 16,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Mob0"
+}, {
+    "name": "NIKE Shox",
+    "company": "NIKe",
+    "img": "img/products/nike-shox-tl.png",
+    "price": "2.850.000",
+    "star": 4,
+    "rateCount": 104,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Mob1"
+}, {
+    "name": "NIKE Dunk",
+    "company": "NIKE",
+    "img": "img/products/nike-sp-dunk.png",
+    "price": "2.490.000",
+    "star": 4,
+    "rateCount": 80,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Mob2"
+}, {
+    "name": "ADIDAS Pro",
+    "company": "ADIDAS",
+    "img": "img/products/adidas-prophere.png",
+    "price": "260.000",
+    "star": 5,
+    "rateCount": 87,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Mob3"
+}, {
+    "name": "ADIDAS Black White",
+    "company": "ADIDAS",
+    "img": "img/products/adidas-prophere-black-white.png",
+    "price": "5.690.000",
+    "star": 4,
+    "rateCount": 372,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Xia2"
+}, {
+    "name": "ADIDAS Super Star Red",
+    "company": "ADIDAS",
+    "img": "img/products/adidas-super-star-red.png",
+    "price": "4.790.000",
+    "star": 4,
+    "rateCount": 347,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Xia3"
+}, {
+    "name": "ADIDAS Swift",
+    "company": "Huawei",
+    "img": "img/products/adidas-swift-run.png",
+    "price": "21.990.000",
+    "star": 5,
+    "rateCount": 4,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Hua0"
+}, {
+    "name": "VANS Black",
+    "company": "VANS",
+    "img": "img/products/vans-black-black.png",
+    "price": "9.990.000",
+    "star": 4,
+    "rateCount": 22,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Hua1"
+}, {
+    "name": "VANS Old School",
+    "company": "Vans",
+    "img": "img/products/van-old-school.png",
+    "price": "1.990.000",
+    "star": 5,
+    "rateCount": 54,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "screen": "39cm",
+        "os": "23.5cm",
+        "camara": "34-35cm",
+        "camaraFront": "20.8cm",
+        "cpu": "6",
+        "ram": "23.5cm",
+        "rom": "4",
+        "microUSB": "20.8cm",
+        "battery": "Một cây thước đo 30cm hoặc thước dây"
+    },
+    "masp": "Hua2"
+}, 
+
+]
